cc9b1ccb140132dd4e6aa9dc3bbdfbdabb90a6be
shelm <dominik_schiller@gmx.net>
2018-07-26 13:34:07 +0200

    Cleaning up

--- code/example/premade_estimator.py
+++ code/example/premade_estimator.py
@@ -11,6 +11,11 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+
+### SOURCE ###
+# https://github.com/tensorflow/tensorflow/models/samples/core/get_started
+
 """An Example of a DNNClassifier for the Iris dataset."""
 from __future__ import absolute_import
 from __future__ import division
