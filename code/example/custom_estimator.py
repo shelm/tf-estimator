db5d88e878c5a88692853c18c9e01a7b4f709b80
shelm <dominik_schiller@gmx.net>
2018-07-26 13:34:07 +0200

    Cleanup

--- code/example/custom_estimator.py
+++ code/example/custom_estimator.py
@@ -11,6 +11,10 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+#### SOURCE ###
+#https://github.com/tensorflow/models/tree/master/samples/core/get_started
+
 """An Example of a custom Estimator for the Iris dataset."""
 from __future__ import absolute_import
 from __future__ import division
